2983f2e4872785c2a0c4d0dfb7a3f56b23aacc6f
Michal Jastak <warlock@ais.pl>
2013-03-24 07:45:00 +0100

    Specification<C> should be applicable to any subclass of C

--- src/main/java/pl/ais/commons/domain/specification/Specification.java
+++ src/main/java/pl/ais/commons/domain/specification/Specification.java
@@ -3,21 +3,23 @@ package pl.ais.commons.domain.specification;
 import javax.annotation.Nullable;
 
 /**
- * Defines the API contract for Specification.
+ * Defines the API contract for the Specification.
  *
  * @see <a href="http://en.wikipedia.org/wiki/Specification_pattern">Specification Pattern</a>
- * @param <T> determines the type of candidate
+ *
+ * @param <C> determines the type of candidate
  * @author Warlock, AIS.PL
  * @since 1.0.1
  */
-public interface Specification<T> {
+public interface Specification<C> {
 
     /**
      * Verifies if this specification is matched by the candidate.
      *
+     * @param <T> determines the upper bound for the candidate type
      * @param candidate the candidate to verify
      * @return {@code true} if this specification is matched by the candidate, {@code false} otherwise
      */
-    boolean isSatisfiedBy(@Nullable T candidate);
+    <T extends C> boolean isSatisfiedBy(@Nullable T candidate);
 
 }
