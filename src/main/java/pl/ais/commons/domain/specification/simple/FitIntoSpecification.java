819afced75edef2c72ee58f1d868cfa5bb22b86d
Michal Jastak <warlock@ais.pl>
2013-03-24 08:03:53 +0100

    Method signature changes (as in Specification); equals/hashCode/toString methods overridden

--- src/main/java/pl/ais/commons/domain/specification/simple/FitIntoSpecification.java
+++ src/main/java/pl/ais/commons/domain/specification/simple/FitIntoSpecification.java
@@ -1,5 +1,10 @@
 package pl.ais.commons.domain.specification.simple;
 
+import javax.annotation.concurrent.Immutable;
+
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
 import pl.ais.commons.domain.specification.Specification;
 
 /**
@@ -9,6 +14,7 @@ import pl.ais.commons.domain.specification.Specification;
  * @author Warlock, AIS.PL
  * @since 1.0.1
  */
+@Immutable
 public final class FitIntoSpecification implements Specification<CharSequence> {
 
     private final int upperLimit;
@@ -27,8 +33,37 @@ public final class FitIntoSpecification implements Specification<CharSequence> {
      * {@inheritDoc}
      */
     @Override
-    public boolean isSatisfiedBy(final CharSequence candidate) {
+    public boolean equals(final Object object) {
+        boolean result = (this == object);
+        if (!result && (null != object) && (getClass() == object.getClass())) {
+            final FitIntoSpecification other = (FitIntoSpecification) object;
+            result = (upperLimit == other.upperLimit);
+        }
+        return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(upperLimit).toHashCode();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <T extends CharSequence> boolean isSatisfiedBy(final T candidate) {
         return (null == candidate) || (candidate.length() <= upperLimit);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("upperLimit", upperLimit).build();
     }
 
 }
